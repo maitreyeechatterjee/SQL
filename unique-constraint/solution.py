9f1b1471909601acb62fba6572dd376fb3f8aff5
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-21 19:35:46 +0530

    Add solution for Unique Constraint

---
+++ unique-constraint/solution.py
@@ -0,0 +1,16 @@
+CREATE TABLE students (CREATE TABLE students (
+
+
+
+
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+SELECT SELECT 
+    column_name,     column_name, 
+    is_nullable,    is_nullable,
+    column_default,    column_default,
+    id INTEGER UNIQUE NOT NULL,    id INTEGER UNIQUE NOT NULL,
+););
+    name TEXT NOT NULL,    name TEXT NOT NULL,
+    age INTEGER NOT NULL    age INTEGER NOT NULL
