a5f960bbc3406cfe603df47627aacb8344ea97ee
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-21 20:09:09 +0530

    Update solution for Foreign Key

--- foreign-key/solution.py
+++ foreign-key/solution.py
@@ -1,10 +1,25 @@
-
-CREATE TABLE employess (CREATE TABLE employess (
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
-    name TEXT,    name TEXT,
+););
     department_id INTEGER REFERENCES departments(id)    department_id INTEGER REFERENCES departments(id)
+    name TEXT,    name TEXT,
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+CREATE TABLE employess (CREATE TABLE employess (
+
 ););
+    name TEXT    name TEXT
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+CREATE TABLE departments (CREATE TABLE departments (
 
 
 
-););
+
+
+
+
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+SELECT SELECT 
+    c.table_name,    c.table_name,
+    c.column_name,     c.column_name, 
+    c.data_type,    c.data_type,
+    CASE     CASE 
