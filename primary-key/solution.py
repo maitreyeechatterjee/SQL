24a77c459a8430dc3db1dedd0a9653628619672d
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-21 19:50:57 +0530

    Update solution for Primary Key

--- primary-key/solution.py
+++ primary-key/solution.py
@@ -1,8 +1,10 @@
-    owner_id INTEGER    owner_id INTEGER
-    title TEXT     title TEXT 
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
 CREATE TABLE videos (CREATE TABLE videos (
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+    title TEXT,    title TEXT,
+    owner_id INTEGER    owner_id INTEGER
+););
+
+
 
 ););
     username TEXT     username TEXT 
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
