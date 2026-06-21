3620cd3aebc346d30366d1e5f36613626fea24e6
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-21 16:47:10 +0530

    Update solution for Create Table

--- create-table/solution.py
+++ create-table/solution.py
@@ -1,17 +1,8 @@
+  id INTEGER,  id INTEGER,
 CREATE TABLE videos (CREATE TABLE videos (
 
-
-
-
-
-
-
--- Do not modify below this line ---- Do not modify below this line --
-SELECT table_name, column_name, data_typeSELECT table_name, column_name, data_type
-FROM information_schema.columnsFROM information_schema.columns
-    id INT,    id INT,
+  name TEXT,  name TEXT,
+  created_at DATE,  created_at DATE,
+  published BOOLEAN  published BOOLEAN
 ););
-WHERE table_name = 'videos';WHERE table_name = 'videos';
-    name TEXT,    name TEXT,
-    created_at DATE,    created_at DATE,
-    published BOOLEUN    published BOOLEUN
+
