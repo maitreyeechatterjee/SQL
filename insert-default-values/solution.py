cb44945202c7c990c125191797121968784b3873
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-22 18:18:25 +0530

    Update solution for Insert Default Values

--- insert-default-values/solution.py
+++ insert-default-values/solution.py
@@ -2,8 +2,9 @@
   name TEXT,  name TEXT,
   stock INTEGER DEFAULT 0  stock INTEGER DEFAULT 0
 ););
-INSERT INTO products (id, name, stock)INSERT INTO products (id, name, stock)
+INSERT INTO products (id, name,)INSERT INTO products (id, name,)
 -- Do not modify above this line ---- Do not modify above this line --
 VALUES (1, 'Apple'), (2, 'Banana'), (3, 'Orange');VALUES (1, 'Apple'), (2, 'Banana'), (3, 'Orange');
 
 
+CREATE TABLE products (CREATE TABLE products (
