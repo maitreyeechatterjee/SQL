8cf3989f9eec3a3c88ef7c4de86b884d2b45a47f
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-22 18:17:21 +0530

    Add solution for Insert Default Values

---
+++ insert-default-values/solution.py
@@ -0,0 +1,18 @@
+CREATE TABLE products (CREATE TABLE products (
+  id INTEGER PRIMARY KEY,  id INTEGER PRIMARY KEY,
+  name TEXT,  name TEXT,
+  stock INTEGER DEFAULT 0  stock INTEGER DEFAULT 0
+););
+INSERT INTO products (id, name, stock)INSERT INTO products (id, name, stock)
+-- Do not modify above this line ---- Do not modify above this line --
+
+
+
+
+
+
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+SELECT * FROM products;SELECT * FROM products;
+VALUES (1, 'Apple', 0), (2, 'Banana',0), (3, 'Orange',0);VALUES (1, 'Apple', 0), (2, 'Banana',0), (3, 'Orange',0);
