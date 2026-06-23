e928a55b9a97bcc84c00fcec2eca62c902f870c9
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 22:56:25 +0530

    Add solution for Variable Types

---
+++ variable-types/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Variable Types
+URL: https://neetcode.io/problems/python-variable-types/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+integer_type=18integer_type=18
+float_type=23.6float_type=23.6
+bool_type=Truebool_type=True
+string_type="Hello"string_type="Hello"
+list_type=[1,23,4]list_type=[1,23,4]
+print(type(integer_type))print(type(integer_type))
+print(type(float_type))print(type(float_type))
+print(type(bool_type))print(type(bool_type))
