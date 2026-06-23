cb30929537630a6fcad667a808617ea24f21c413
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:22:48 +0530

    Add solution for Variable Naming

---
+++ variable-naming/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Variable Naming
+URL: https://neetcode.io/problems/python-variable-naming/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+capital_of_spain = "Madrid"capital_of_spain = "Madrid"
+capital_of_france = "Paris"capital_of_france = "Paris"
+
+
+print(capital_of_spain)print(capital_of_spain)
+print(capital_of_france)print(capital_of_france)
+capital_of_germany = "Berlin"capital_of_germany = "Berlin"
+print(capital_of_germany)print(capital_of_germany)
