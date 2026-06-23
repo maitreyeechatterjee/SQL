96a81c53340141a5314b1dcc5382d0733b81155b
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:13:09 +0530

    Add solution for Variable Declaration

---
+++ variable-declaration/solution.py
@@ -0,0 +1,12 @@
+"""
+Problem: Variable Declaration
+URL: https://neetcode.io/problems/python-variable-declaration/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+message ="this string is stored in a variable"message ="this string is stored in a variable"
+# don't modify the code below this line# don't modify the code below this line
+print(message)print(message)
+print(message)print(message)
