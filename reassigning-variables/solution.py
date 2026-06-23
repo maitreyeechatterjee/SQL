d71cffb681c4925f1225f981f02f5ec8eaad68fa
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 22:33:24 +0530

    Add solution for Reassigning Variables

---
+++ reassigning-variables/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: Reassigning Variables
+URL: https://neetcode.io/problems/python-reassigning-variables/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+message = "The first message"message = "The first message"
+
+print(message)print(message)
+
+print(message)print(message)
+
+message = "The third message"message = "The third message"
+
+print(message)print(message)
+
+message = "The second message"message = "The second message"
