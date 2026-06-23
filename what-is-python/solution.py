6928fdd953013dfb98f0235f740b6031d65c2290
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 20:40:01 +0530

    Add solution for What is Python?

---
+++ what-is-python/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: What is Python?
+URL: https://neetcode.io/problems/python-what-is-python/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+    getcontext().prec = n + 2  # Set precision higher than needed for accuracy    getcontext().prec = n + 2  # Set precision higher than needed for accuracy
+        
+    C = 426880 * Decimal(10005).sqrt()    C = 426880 * Decimal(10005).sqrt()
+    K = 6    K = 6
+    M = 1    M = 1
+    X = 1    X = 1
+    L = 13591409    L = 13591409
