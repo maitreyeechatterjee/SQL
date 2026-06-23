796e5c5fc800cab4851c7888f94935a68d6bac61
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 23:17:35 +0530

    Add solution for Type Errors

---
+++ type-errors/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Type Errors
+URL: https://neetcode.io/problems/python-type-errors/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+variable = "10"variable = "10"
+
+# Don't modify the code below this line# Don't modify the code below this line
+print(variable)print(variable)
+print(type(variable))print(type(variable))
+
+variable = int(variable)variable = int(variable)
+
+print(variable)print(variable)
