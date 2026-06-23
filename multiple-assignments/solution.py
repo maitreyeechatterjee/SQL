200dde22f49eb5b74ef1d40c624a01ac245d3591
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 22:46:26 +0530

    Add solution for Multiple Assignments

---
+++ multiple-assignments/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: Multiple Assignments
+URL: https://neetcode.io/problems/python-multiple-assignment/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+msg1, msg2 = "World", "Hello"msg1, msg2 = "World", "Hello"
+msg3, msg4, msg5 = "Name", "Is", "My"msg3, msg4, msg5 = "Name", "Is", "My"
+# Don't change the code above this line# Don't change the code above this line
+msg1,msg2,msg3,msg4,msg5 = msg2,msg1,msg5,msg3,msg4msg1,msg2,msg3,msg4,msg5 = msg2,msg1,msg5,msg3,msg4
+
+
+
+# Don't change the code below this line# Don't change the code below this line
+print(msg1)print(msg1)
+print(msg2)print(msg2)
+print(msg3)print(msg3)
+print(msg4)print(msg4)
+print(msg5)print(msg5)
