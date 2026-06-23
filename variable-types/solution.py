243b8648573bf1980521b81d5bbf48db133193de
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 23:00:56 +0530

    Update solution for Variable Types

--- variable-types/solution.py
+++ variable-types/solution.py
@@ -1,6 +1,6 @@
 """
 Problem: Variable Types
-URL: https://neetcode.io/problems/python-variable-types/question
+URL: https://neetcode.io/problems/python-variable-types/solution
 Language: python
 
 Solution by NeetCode GitHub Pusher
@@ -12,5 +12,5 @@ integer_type = 2integer_type = 2
 
 float_type = 2.2float_type = 2.2
 bool_type = Falsebool_type = False
-str_type = "hey there"str_type = "hey there"
+string_type = "hey there"string_type = "hey there"
 list_type = [1,2,3,4]list_type = [1,2,3,4]
