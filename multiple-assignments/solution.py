38bd10d96cedc8de85b27cf80de2ae555b3db32c
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 22:49:38 +0530

    Update solution for Multiple Assignments

--- multiple-assignments/solution.py
+++ multiple-assignments/solution.py
@@ -6,8 +6,8 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-msg1, msg2 = "World", "Hello"msg1, msg2 = "World", "Hello"
-msg3, msg4, msg5 = "Name", "Is", "My"msg3, msg4, msg5 = "Name", "Is", "My"
+msg1, msg2 = "Hello", "World"msg1, msg2 = "Hello", "World"
+msg3, msg4, msg5 = "My", "Name", "Is"msg3, msg4, msg5 = "My", "Name", "Is"
 # Don't change the code above this line# Don't change the code above this line
 
 
