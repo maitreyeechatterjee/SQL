27ac3e45562de1e3afc2f09239e9d590842b8bd6
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 23:11:53 +0530

    Add solution for Type Casting

---
+++ type-casting/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Type Casting
+URL: https://neetcode.io/problems/python-type-casting/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+pi = 3.141592653589793pi = 3.141592653589793
+square_root_8 = 2.8284271247461903square_root_8 = 2.8284271247461903
+# Do not modify above this line# Do not modify above this line
+pi = int(pi)pi = int(pi)
+square_root_8= int(square_root_8)square_root_8= int(square_root_8)
+
+# Do not modify below this line# Do not modify below this line
+print(pi)print(pi)
+print(square_root_8)print(square_root_8)
