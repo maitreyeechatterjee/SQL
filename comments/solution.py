d22032cb0de7ab8f54eb6206662e17530b2434b4
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:04:14 +0530

    Update solution for Comments

--- comments/solution.py
+++ comments/solution.py
@@ -7,7 +7,7 @@ Solution by NeetCode GitHub Pusher
 """
 
 print("I belong here.")print("I belong here.")
-print("Why am I here?")print("Why am I here?")
-print("Get me out of here!")print("Get me out of here!")
+# print("Why am I here?")# print("Why am I here?")
+# print("Get me out of here!")# print("Get me out of here!")
 print("I also belong here.")print("I also belong here.")
 print("Me too!")print("Me too!")
