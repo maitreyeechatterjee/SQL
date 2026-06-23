7df893fdb74e706330e227691b3cd64c445337f8
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 20:43:42 +0530

    Add solution for Execution Order

---
+++ execution-order/solution.py
@@ -0,0 +1,11 @@
+"""
+Problem: Execution Order
+URL: https://neetcode.io/problems/python-execution-order/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+print("Fifth")print("Fifth")
+print("Fourth")print("Fourth")
+print("Sixth")print("Sixth")
