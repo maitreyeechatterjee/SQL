47bc72ca17752b7acd6ae62c3ed5b2f3b335d04a
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:03:53 +0530

    Add solution for Comments

---
+++ comments/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Comments
+URL: https://neetcode.io/problems/python-comments/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+print("I belong here.")print("I belong here.")
+print("Why am I here?")print("Why am I here?")
+print("Get me out of here!")print("Get me out of here!")
+print("I also belong here.")print("I also belong here.")
+print("Me too!")print("Me too!")
