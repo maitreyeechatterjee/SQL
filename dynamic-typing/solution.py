d58dff36d515d40e9f146621513b6549407680d5
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 23:09:23 +0530

    Add solution for Dynamic Typing

---
+++ dynamic-typing/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Dynamic Typing
+URL: https://neetcode.io/problems/python-dynamic-typing/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+
+print(type(70))print(type(70))
+
+print(type(7.7))print(type(7.7))
+
+print(type(True))print(type(True))
+
+print(type("hello there"))print(type("hello there"))
