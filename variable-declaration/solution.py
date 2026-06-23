e96907a20edc28538745cb112833479c0662870e
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:13:47 +0530

    Update solution for Variable Declaration

--- variable-declaration/solution.py
+++ variable-declaration/solution.py
@@ -10,3 +10,4 @@ message ="this string is stored in a variable"message ="this string is
 # don't modify the code below this line# don't modify the code below this line
 print(message)print(message)
 print(message)print(message)
+print(message)print(message)
