b76baefffc770ec625a8f7e5e6ea55c52cbf7138
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-23 21:38:31 +0530

    Add solution for Naming Conventions

---
+++ naming-conventions/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: Naming Conventions
+URL: https://neetcode.io/problems/python-naming-conventions/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+python_creation_date = "February, 1991"python_creation_date = "February, 1991"
+java_creation_date = "May, 1995"java_creation_date = "May, 1995"
+javascript_creation_date = "December, 1995"javascript_creation_date = "December, 1995"
+
+print(python_creation_date)print(python_creation_date)
+print(java_creation_date)print(java_creation_date)
+print(javascript_creation_date)print(javascript_creation_date)
