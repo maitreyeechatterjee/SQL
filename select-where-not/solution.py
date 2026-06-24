63738181841617a8c381b0e3774a5813fdebe33f
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-24 21:51:07 +0530

    Add solution for Select Where Not

---
+++ select-where-not/solution.py
@@ -0,0 +1,21 @@
+CREATE TABLE athletes (CREATE TABLE athletes (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,    name TEXT,
+    sport TEXT    sport TEXT
+););
+
+INSERT INTO athletes (name, sport) VALUESINSERT INTO athletes (name, sport) VALUES
+  ('LeBron James', 'Basketball'),  ('LeBron James', 'Basketball'),
+  ('Serena Williams', 'Tennis'),  ('Serena Williams', 'Tennis'),
+  ('Michael Jordan', 'Basketball'),  ('Michael Jordan', 'Basketball'),
+  ('Kobe Bryant', 'Basketball'),  ('Kobe Bryant', 'Basketball'),
+  ('Cristiano Ronaldo', 'Soccer'),  ('Cristiano Ronaldo', 'Soccer'),
+  ('Lionel Messi', 'Soccer'),  ('Lionel Messi', 'Soccer'),
+  ('Tom Brady', 'Football'),  ('Tom Brady', 'Football'),
+  ('Usain Bolt', 'Track and Field');  ('Usain Bolt', 'Track and Field');
+-- Do not modify above this line. ---- Do not modify above this line. --
+
+SELECT name, FROM athletes WHERE sport != 'Basketball'SELECT name, FROM athletes WHERE sport != 'Basketball'
+
+
+
