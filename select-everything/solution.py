a761f9ffc47879398da8c74a56f7430843196006
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-24 20:44:09 +0530

    Update solution for Select Everything

--- select-everything/solution.py
+++ select-everything/solution.py
@@ -1,15 +1,8 @@
-CREATE TABLE coding_problems (CREATE TABLE coding_problems (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name TEXT,    name TEXT,
-    difficulty TEXT,    difficulty TEXT,
-    description TEXT    description TEXT
-););
-SELECT * FROM coding_problems;SELECT * FROM coding_problems;
+
 INSERT INTO coding_problems (name, difficulty, description) VALUESINSERT INTO coding_problems (name, difficulty, description) VALUES
     ('Dynamic Array', 'Easy', 'Implement a dynamic array in C++'),    ('Dynamic Array', 'Easy', 'Implement a dynamic array in C++'),
     ('Binary Search Tree', 'Medium', 'Implement a binary search tree in C++'),    ('Binary Search Tree', 'Medium', 'Implement a binary search tree in C++'),
     ('Graph Traversal', 'Hard', 'Implement depth-first search and breadth-first search in C++');    ('Graph Traversal', 'Hard', 'Implement depth-first search and breadth-first search in C++');
 -- Do not modify above this line. ---- Do not modify above this line. --
 
-
-
+SELECT * FROM coding_problems;SELECT * FROM coding_problems;
