20ea8da94335dc641a2f9969b0a509f115d689a4
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-24 20:57:22 +0530

    Update solution for Select Columns

--- select-columns/solution.py
+++ select-columns/solution.py
@@ -1,8 +1,22 @@
+CREATE TABLE comments (CREATE TABLE comments (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    author TEXT,    author TEXT,
+    body TEXT,    body TEXT,
+    posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+););
+
+INSERT INTO comments (author, body) VALUESINSERT INTO comments (author, body) VALUES
+    ('NeetCode', 'This problem should be marked as easy.. haha'),    ('NeetCode', 'This problem should be marked as easy.. haha'),
+    ('Lee', 'This problem is too hard for an easy tag');    ('Lee', 'This problem is too hard for an easy tag');
+
+  
 SELECT author AS comment_author, body AS comment_bodySELECT author AS comment_author, body AS comment_body
 FROM comments;FROM comments;
 
-  
 
-    ('Lee', 'This problem is too hard for an easy tag');    ('Lee', 'This problem is too hard for an easy tag');
-    ('NeetCode', 'This problem should be marked as easy.. haha'),    ('NeetCode', 'This problem should be marked as easy.. haha'),
-INSERT INTO comments (author, body) VALUESINSERT INTO comments (author, body) VALUES
+
+-- Do not modify above this line. ---- Do not modify above this line. --
+
+SELECT author AS comment_author, body AS comment_body;SELECT author AS comment_author, body AS comment_body;
+
+
