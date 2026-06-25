2c06e9c7a360f60b78d433362cb2d730095aa2b8
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-25 21:09:56 +0530

    Update solution for Select Like

--- select-like/solution.py
+++ select-like/solution.py
@@ -1,18 +1,3 @@
-CREATE TABLE cities (CREATE TABLE cities (
-    id INT PRIMARY KEY,    id INT PRIMARY KEY,
-    name VARCHAR(255),    name VARCHAR(255),
-    country VARCHAR(255)    country VARCHAR(255)
-););
-
-INSERT INTO cities (id, name, country) VALUESINSERT INTO cities (id, name, country) VALUES
-(1, 'Tokyo', 'Japan'),(1, 'Tokyo', 'Japan'),
-(2, 'London', 'England'),(2, 'London', 'England'),
-(3, 'Paris', 'France'),(3, 'Paris', 'France'),
-(4, 'New York', 'USA'),(4, 'New York', 'USA'),
-(5, 'New Delhi', 'India'),(5, 'New Delhi', 'India'),
-(6, 'Shanghai', 'China'),(6, 'Shanghai', 'China'),
-(7, 'Sao Paulo', 'Brazil'),(7, 'Sao Paulo', 'Brazil'),
-(8, 'Mexico City', 'Mexico'),(8, 'Mexico City', 'Mexico'),
 (9, 'Cairo', 'Egypt'),(9, 'Cairo', 'Egypt'),
 (10, 'Mumbai', 'India'),(10, 'Mumbai', 'India'),
 (11, 'Beijing', 'China'),(11, 'Beijing', 'China'),
@@ -20,4 +5,4 @@ INSERT INTO cities (id, name, country) VALUESINSERT INTO cities (id, n
 (13, 'New Orleans', 'USA');(13, 'New Orleans', 'USA');
 -- Do not modify above this line. ---- Do not modify above this line. --
 
-SELECT name FROM cities WHERE name LIKE 'ai%'SELECT name FROM cities WHERE name LIKE 'ai%'
+SELECT name FROM cities WHERE name LIKE '%ai%'SELECT name FROM cities WHERE name LIKE '%ai%'
