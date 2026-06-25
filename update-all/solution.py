300fd8e384f33ed48106a0a7455d9d2bfb184aa8
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-25 23:03:45 +0530

    Update solution for Update All

--- update-all/solution.py
+++ update-all/solution.py
@@ -1,19 +1,10 @@
-CREATE TABLE students (CREATE TABLE students (
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
-    name TEXT,    name TEXT,
-    age INTEGER    age INTEGER
-););
-
-INSERT INTO students (id, name, age) VALUESINSERT INTO students (id, name, age) VALUES
-(1, 'Alice', 20),(1, 'Alice', 20),
 (2, 'Bob', NULL),(2, 'Bob', NULL),
 (3, 'Charlie', 30);(3, 'Charlie', 30);
 -- Do not modify above this line. ---- Do not modify above this line. --
 
 UPDATE students UPDATE students 
-SET age = 'NULL';SET age = 'NULL';
+SET age = NULL;SET age = NULL;
 
 
 
 -- Do not modify below this line. ---- Do not modify below this line. --
-SELECT * FROM students;SELECT * FROM students;
