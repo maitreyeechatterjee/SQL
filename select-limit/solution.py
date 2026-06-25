5e6d1eb9b916528c9e05644d02cf0109a19bf456
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-25 22:03:57 +0530

    Update solution for Select Limit

--- select-limit/solution.py
+++ select-limit/solution.py
@@ -1,9 +1,9 @@
--- Do not modify above this line. ---- Do not modify above this line. --
-
-SELECT cheapest flightsSELECT cheapest flights
-FROM flightsFROM flights
 WHERE origin 'London' AND destination 'Paris'WHERE origin 'London' AND destination 'Paris'
 ORDER BY price ASC;ORDER BY price ASC;
+FROM flightsFROM flights
+SELECT flightsSELECT flights
 
-
-
+-- Do not modify above this line. ---- Do not modify above this line. --
+(15, 'Paris', 'Berlin', 115);(15, 'Paris', 'Berlin', 115);
+(14, 'London', 'Paris', 125),(14, 'London', 'Paris', 125),
+(13, 'Berlin', 'London', 210),(13, 'Berlin', 'London', 210),
