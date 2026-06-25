32770e66088bcedf026c8b911486b97943863a3f
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-25 23:02:38 +0530

    Update solution for Update Where

--- update-where/solution.py
+++ update-where/solution.py
@@ -1,24 +1,9 @@
-CREATE TABLE users (CREATE TABLE users (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username TEXT    username TEXT
-););
+WHERE username IS NULL; WHERE username IS NULL; 
 
-INSERT INTO users (username) VALUESINSERT INTO users (username) VALUES
-  ('Alice'),  ('Alice'),
-  ('Bob'),  ('Bob'),
-  (NULL),  (NULL),
-  ('Charlie'),  ('Charlie'),
-  (NULL);  (NULL);
 
--- Do not modify above this line. ---- Do not modify above this line. --
 
+SET username = 'anonymous'SET username = 'anonymous'
 UPDATE users UPDATE users 
 
-
-
-
-
--- Do not modify below this line. ---- Do not modify below this line. --
-SELECT * FROM users;SELECT * FROM users;
-SET username = anonymousSET username = anonymous
-WHERE username IS NULL; WHERE username IS NULL; 
+-- Do not modify above this line. ---- Do not modify above this line. --
+  (NULL);  (NULL);
