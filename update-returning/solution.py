7b90bf9066b823606095baa5b6d093e02f08c795
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-25 23:08:55 +0530

    Update solution for Update Returning

--- update-returning/solution.py
+++ update-returning/solution.py
@@ -2,8 +2,9 @@
 
 UPDATE bank_accountsUPDATE bank_accounts
 SET status = 'VIP'SET status = 'VIP'
-WHERE balance > 10000 AND < 100000WHERE balance > 10000 AND < 100000
+WHERE balance > 10000 AND balance < 100000WHERE balance > 10000 AND balance < 100000
 RETURNING id, balance, status;RETURNING id, balance, status;
 
 
 
+(60000, 'Frank');(60000, 'Frank');
