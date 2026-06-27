9ab86716074a4315f5f97132f977842091a74c2b
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-27 21:46:10 +0530

    Update solution for Delete All

--- delete-all/solution.py
+++ delete-all/solution.py
@@ -7,3 +7,13 @@ CREATE TABLE students (CREATE TABLE students (
 INSERT INTO students (name, age)INSERT INTO students (name, age)
   VALUES ('John Doe', 20),  VALUES ('John Doe', 20),
          ('Jane Doe', 21),         ('Jane Doe', 21),
+         ('John Smith', 22),         ('John Smith', 22),
+         ('Jane Smith', 23);         ('Jane Smith', 23);
+-- Do not modify above this line. ---- Do not modify above this line. --
+
+DELETE FROM students;DELETE FROM students;
+
+
+
+-- Do not modify below this line. ---- Do not modify below this line. --
+SELECT * FROM students;SELECT * FROM students;
