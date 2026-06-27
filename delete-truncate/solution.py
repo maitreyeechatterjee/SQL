4a13c693ef8dce756d870b8578f3e6ab7c1b7889
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-27 23:22:38 +0530

    Add solution for Delete Truncate

---
+++ delete-truncate/solution.py
@@ -0,0 +1,8 @@
+    name TEXT    name TEXT
+););
+
+INSERT INTO students (id, name)INSERT INTO students (id, name)
+  VALUES (1, 'Alice'),  VALUES (1, 'Alice'),
+         (2, 'Bob'),         (2, 'Bob'),
+         (3, 'Charlie');         (3, 'Charlie');
+-- Do not modify above this line. ---- Do not modify above this line. --
