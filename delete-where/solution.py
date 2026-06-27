13bca6e737085c91e905d04fba3efa8fcf5b15f0
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-27 21:51:17 +0530

    Update solution for Delete Where

--- delete-where/solution.py
+++ delete-where/solution.py
@@ -1,21 +1,10 @@
-CREATE TABLE students (CREATE TABLE students (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name TEXT,    name TEXT,
-    age INTEGER    age INTEGER
-););
-
-INSERT INTO students (name, age)INSERT INTO students (name, age)
-  VALUES ('John Doe', 16),  VALUES ('John Doe', 16),
-         ('Jane Doe', 19),         ('Jane Doe', 19),
-         ('Alice Smith', 22),         ('Alice Smith', 22),
-         ('Bob Smith', 23),         ('Bob Smith', 23),
          ('Alice Johnson', 26);         ('Alice Johnson', 26);
 -- Do not modify above this line. ---- Do not modify above this line. --
 
 DELETE FROM students DELETE FROM students 
+WHERE age <18 OR , name = 'A%';WHERE age <18 OR , name = 'A%';
 
 
 
 -- Do not modify below this line. ---- Do not modify below this line. --
-SELECT * FROM students;SELECT * FROM students;
-WHERE age = <18 OR , name = 'A%';WHERE age = <18 OR , name = 'A%';
+         ('Bob Smith', 23),         ('Bob Smith', 23),
