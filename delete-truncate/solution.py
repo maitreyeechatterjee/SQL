f02d105efb62185250e25ae140077e638ee9c84b
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-27 23:24:38 +0530

    Update solution for Delete Truncate

--- delete-truncate/solution.py
+++ delete-truncate/solution.py
@@ -6,3 +6,13 @@ INSERT INTO students (id, name)INSERT INTO students (id, name)
          (2, 'Bob'),         (2, 'Bob'),
          (3, 'Charlie');         (3, 'Charlie');
 -- Do not modify above this line. ---- Do not modify above this line. --
+
+DELETE FROM Students;DELETE FROM Students;
+
+
+
+-- Do not modify below this line. ---- Do not modify below this line. --
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+SELECT * FROM students;SELECT * FROM students;
+
+CREATE TABLE students (CREATE TABLE students (
