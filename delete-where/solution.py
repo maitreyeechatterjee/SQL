d3ae98ea68672c7480d315565e840140bdba72b1
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-06-27 21:52:23 +0530

    Update solution for Delete Where

--- delete-where/solution.py
+++ delete-where/solution.py
@@ -1,10 +1,10 @@
-         ('Alice Johnson', 26);         ('Alice Johnson', 26);
--- Do not modify above this line. ---- Do not modify above this line. --
 
 DELETE FROM students DELETE FROM students 
-WHERE age < 18 OR name = 'A%';WHERE age < 18 OR name = 'A%';
+WHERE age < 18 OR name LIKE 'A%';WHERE age < 18 OR name LIKE 'A%';
 
 
 
 -- Do not modify below this line. ---- Do not modify below this line. --
+-- Do not modify above this line. ---- Do not modify above this line. --
+         ('Alice Johnson', 26);         ('Alice Johnson', 26);
          ('Bob Smith', 23),         ('Bob Smith', 23),
